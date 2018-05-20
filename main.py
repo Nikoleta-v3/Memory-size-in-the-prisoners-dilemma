94e5c7a239eac56d2d3c581cdd3ec43b21dc954f
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-05-20 17:40:51 +0100

    main v2
    The script has been tweaked such that for each method a number of
    diffent parameters are used in both training and optimisation.
    
    Moreover, now all files are written in the same data frame
    and exported as method_seed.

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def prepare_objective_optimisation(opponents):
 
 def objective_score(pattern, turns, repetitions, opponents, params):
     """Objective function to maximize total score over matches."""
-    print('Here')
     parameters = Plays(self_plays=params[0], op_plays=params[1], op_openings=params[2])
     size = pattern_size(params)
 
@@ -54,7 +53,6 @@ def objective_score(pattern, turns, repetitions, opponents, params):
     tournament = axl.Tournament(players=players, turns=turns, edges=edges,
                                 repetitions=repetitions)
     results = tournament.play(progress_bar=False)
-    print('and here')
     return - np.mean(results.normalised_scores[-1])
 
 def pattern_size(params):
@@ -66,28 +64,22 @@ def pattern_size(params):
 
     return len(list(keys))
 
-def optimal_memory_one(method, opponents, turns, repetitions, n_calls=40,
-                       n_random_starts=20, popsize=100, strategy='best1bin', 
-                       mutation=.2):
+def optimal_memory_one(method, opponents, turns, repetitions, method_params):
     """
     Approximates the best memory one strategy for the given environment.
     Returns the strategy, the utility u and U.
     """
     bounds = [(0, 0.9999) for _ in range(4)]
-    seed = 0
-    random_state = 0
     objective = prepare_objective_optimisation(opponents=opponents)
 
     if method == 'bayesian':
-        result = skopt.gp_minimize(objective, bounds,
-                                   acq_func="EI",
-                                   n_calls=n_calls,
-                                   n_random_starts=n_random_starts,
-                                   random_state=random_state)
+        result = skopt.gp_minimize(func=objective, dimensions=bounds,
+                                   acq_func="EI", random_state=0, **method_params)
     if method == 'differential':
+        print('start')
         result = scipy.optimize.differential_evolution(func=objective, bounds=bounds,
-                                                       strategy=strategy, popsize=popsize,
-                                                       seed=seed, mutation=mutation)
+                                                       strategy='best1bin', seed=0,
+                                                       **method_params)
 
     best_response = list(result.x)
 
@@ -104,78 +96,77 @@ def optimal_memory_one(method, opponents, turns, repetitions, n_calls=40,
 
     return best_response, -result.fun, score
 
-def train_gambler(method, opponents, turns, repetitions, params, n_calls=20,
-                  n_random_starts=20, popsize=50, strategy='best1bin', mutation=.2):
+def train_gambler(method, opponents, turns, repetitions, params, method_params):
     """
     Approximates the best gambler for the given environment.
     Returns the strategy and it's utility.
     """
     size = pattern_size(params)
     bounds = [(0.0, .99999) for _ in range(size + 1)]
-    seed = 0
-    random_state = 0
     objective = prepare_objective_training(turns=turns, repetitions=repetitions,
-                                          opponents=opponents, params=params)
+                                           opponents=opponents, params=params)
 
     if method == 'bayesian':
-        result = skopt.gp_minimize(objective, bounds,
-                                   acq_func="EI",
-                                   n_calls=n_calls,
-                                   n_random_starts=n_random_starts,
-                                   random_state=random_state)
+        result = skopt.gp_minimize(func=objective, dimensions=bounds,
+                                   acq_func="EI", random_state=0, **method_params)
     if method == 'differential':
+        print('start')
         result = scipy.optimize.differential_evolution(func=objective, bounds=bounds,
-                                                       strategy=strategy, popsize=popsize,
-                                                       seed=seed, mutation=mutation)
+                                                       strategy='best1bin', seed=0,
+                                                       **method_params)
     return result.x, -result.fun
 
-def get_filename(location, folder, params, index):
+def get_filename(location, params, index, method):
     filename = location + 'gambler{}_{}_{}/'.format(params[0], params[1], params[2])
-    filename += folder + '/{}.csv'.format(index)
+    filename += '{}_{}.csv'.format(method, index)
     return filename
 
-def get_columns(params):
+def get_columns(params, method_params):
     cols = ['index', '$q_1$', '$q_2$', '$q_3$', '$q_4$', r'$\bar{q}_1$', r'$\bar{q}_2$',
             r'$\bar{q}_3$', r'$\bar{q}_4$', '$p_1$', '$p_2$', '$p_3$', '$p_4$',
             '$u_q$', '$U_q$', 'Optimisation time', '$U_{G}$', 'Training time',
             'Gambler Initial']
     size = pattern_size(params)
     gambler_cols = ['Gambler {} key'.format(i) for i in range(size)]
-    method_cols = ['method'] + ['method param {}'.format(i) for i in range(2)]
-    
+    method_cols = ['method'] + ['{}'.format(key) for key in method_params.keys()]
+
     return cols + gambler_cols + method_cols
 
-def write_results(method, method_params, list_opponents, filename, params, turns,
-                  repetitions):
+def write_results(index, method, list_opponents, params, turns, repetitions, method_params):
 
-    cols = get_columns(params)
+    cols = get_columns(params, method_params)
     frame = pd.DataFrame()
 
-    row = [q for player in list_opponents for q in player]
-    if len(row) == 4:
+    row = [index]
+    row += [q for player in list_opponents for q in player]
+    if len(row) == 5:
         for _ in range(4):
             row.append(None)
 
     start_optimisation = time.clock()
     best_response, theoretical, simulated = optimal_memory_one(method, opponents=list_opponents,
-                                                               turns=turns, repetitions=repetitions)
+                                                               turns=turns,repetitions=repetitions,
+                                                               method_params=method_params)
     row += [p for p in best_response]
     row.append(theoretical), row.append(simulated)
     row.append(time.clock() - start_optimisation)
 
     start_training = time.clock()
     opt_gambler, utility = train_gambler(method, opponents=list_opponents, turns=turns,
-                                         repetitions=repetitions, params=params)
-    
+                                         repetitions=repetitions, params=params,
+                                         method_params=method_params)
+
+    row.append(utility), row.append(time.clock() - start_training)
     for vector in opt_gambler:
         row.append(vector)
 
-    row.append(utility), row.append(time.clock() - start_training)
-    row.append(method), row.append(method_params[0]), row.append(method_params[1])
+    row.append(method)
+    for value in method_params.values():
+        row.append(value)
     frame = frame.append([row])
     frame.columns = cols
 
-    frame.to_csv(filename, index=False)
+    return frame
 
 if __name__ == '__main__':
     num_turns = 200
@@ -187,28 +178,52 @@ if __name__ == '__main__':
     num_op_start_plays = int(sys.argv[4])
     method = sys.argv[5]
 
-    location = 'data/random_numerical_experiments/{}/'.format(method)
+    location = 'data/random_numerical_experiments/'
     params = [num_plays, num_op_plays, num_op_start_plays]
 
     i = (index - 1) * 100
     while i <= index * 100:
         axl.seed(i)
         main_op = [np.random.random(4)]
+        dfs = []
+        filename =  get_filename(location=location, params=params, index=i, method=method)
 
-        # match
-        filename =  get_filename(location=location, folder='matches', params=params,
-                                 index=i)
-        write_results(method=method, list_opponents=main_op, filename=filename, params=params,
-                      turns=num_turns, repetitions=num_repetitions)
+        if method == 'bayesian':
+            for starts, calls in [(10, 20), (20, 30), (20, 40), (20, 45), (20, 50)]:
+                method_params = {'n_random_starts' : starts, 'n_calls': calls}
 
-        # tournament
-        axl.seed(i + 10000)
-        other = [np.random.random(4)]
+                # match
+                dfs.append(write_results(index=i, method=method, list_opponents=main_op,
+                                         params=params, turns=num_turns, repetitions=num_repetitions,
+                                         method_params=method_params))
 
-        opponents = main_op + other
-        filename =  get_filename(location=location, folder='tournaments', params=params,
-                                 index=i)
-        write_results(method=method, list_opponents=opponents, filename=filename,
-                      params=params, turns=num_turns, repetitions=num_repetitions)
+                # tournament
+                axl.seed(i + 10000)
+                other = [np.random.random(4)]
 
+                opponents = main_op + other
+                dfs.append(write_results(index=i, method=method, list_opponents=opponents,
+                                         params=params, turns=num_turns,
+                                         repetitions=num_repetitions, method_params=method_params))
+
+        if method == 'differential':
+            for popsize, mutation_rate in [(5, 0.1), (10, .2), (15, .2), (20, .3), (25, 0.4)]:
+                method_params = {'popsize' : popsize, 'mutation': mutation_rate}
+
+                # match
+                dfs.append(write_results(index=i, method=method, list_opponents=main_op,
+                                         params=params, turns=num_turns, repetitions=num_repetitions,
+                                         method_params=method_params))
+
+                # tournament
+                axl.seed(i + 10000)
+                other = [np.random.random(4)]
+
+                opponents = main_op + other
+                dfs.append(write_results(index=i, method=method, list_opponents=opponents,
+                                         params=params, turns=num_turns,
+                                         repetitions=num_repetitions, method_params=method_params))
+
+        df = pd.concat(dfs, ignore_index=True)
+        df.to_csv(filename)
         i += 1
