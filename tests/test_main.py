15171d0e5121ac071a98efc232b67aeb8d520fbf
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2018-12-11 13:46:51 +0000

    First paper draft (#29)
    * draft
    
    * draft of numericall section
    
    * draft to jump back to numeric
    
    * A good writeup up to purely random analytically
    
    * write up of purely random strategies
    
    This includes the analytical and the numerical section
    
    * reactive analytically
    
    * fix reactive script
    
    * finish algorithm reactive
    
    * reactive numerics sections
    
    * stability of defection
    
    * first draft of paper + reactive script
    
    * morning corrections
    
    * Reactive script (#31)
    
    * rename functions and add docstrings
    
    * tweak implementation and add tests
    
    * Fix script for matches
    
    Find optimal reactive for matches and train gambler
    
    * implement tournaments
    
    * fix path to main.py
    
    * fix stability of defection section
    
    * generate images for reactive players
    
    * Fix reactive script (#32)
    
    * remove function from script.
    
    All functions used in our main script leaved in the same file, main.py.
    The functions have been moved to optimisation so they can be used
    in different scripts or just call them via opt_mo
    
    * no longer need to import main just opt_mo
    
    * fix reactive script
    
    Now utility is being optimised using bayesian algorithm as well.
    grambler is notbeing trained cause it is not needed.
    
    * change title
    
    * minor changes
    
    * fix comments in introduction.
    
    Apart from adding Nowak's article (opened an issue).
    
    * change title of section two
    
    * fix utility section
    
    * fix reactive implementation. (#39)
    
    After performing several numerical trials we noticed that the reactive
    optimisation was not captured best behaviour. This fixes the issue.
    
    The issue was that when p1 is 0 or 1 then we solve only the second
    partial derivative. The derivative which is depended on p2
    
    * dummy
    
    * Last tweaks in tility against memory one players
    
    Fixes plots, Theorem 2 and discuss the validation
    though Stewart and Plotkin's tournament. Table
    with results still need to be added.
    
    * add reslts against stewart and plotkin's tour
    
    * re write best response section
    
    This section has been decicaded to best responses
    against a group and a signle memory one strategy.
    
    The structure (for most) has been discussion, lemma, proof
    and empirical results.
    
    Things to consider are:
    
    - concavity. Is the sum of non concave functions always non concave
    - proofs. Some lemmas follow from a lemma with a proof. Do I need
    to add proofs for these as well.
    
    * draft of stability of defection
    
    * tweak previous section for stability section
    
    * fix stability of defection section.
    
    * draft of optimisation of memory one strategies
    
    * Second draft.
    
    This needs morning coffee and corrections.
    
    * corrections to second draft

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,11 +4,7 @@ main = imp.load_source('main', '../main.py')
 import os
 import unittest
 import numpy as np
-import axelrod as axl
 import pandas as pd
-
-import warnings
-warnings.filterwarnings("ignore")
 
 class TestGetFilename(unittest.TestCase):
 
@@ -29,110 +25,6 @@ class TestGetFilename(unittest.TestCase):
 
         filename = main.get_filename(location, params, index, method)
         self.assertEqual(filename, 'gambler2_1_1/differential_1.csv')
-
-class TestObjectiveScore(unittest.TestCase):
-    turns, repetitions, params = 100, 5, [1, 1, 1]
-
-    def test_against_defector(self):
-        opponent = [(0, 0, 0, 0)]
-        pattern_1 = [0 for _ in range(9)]
-        pattern_2 = [1 for _ in range(9)]
-
-        obj_1 = main.objective_score(pattern=pattern_1, turns=self.turns,
-                                     repetitions=self.repetitions, opponents=opponent,
-                                     params=self.params)
-        obj_2 = main.objective_score(pattern=pattern_2, turns=self.turns,
-                                     repetitions=self.repetitions, opponents=opponent,
-                                     params=self.params)
-        self.assertTrue(np.isclose(abs(obj_1), 1.04))
-        self.assertTrue(np.isclose(abs(obj_2), 0.03))
-
-    def test_against_cooperator(self):
-        opponent = [(1, 1, 1, 1)]
-        pattern_1 = [0 for _ in range(9)]
-        pattern_2 = [1 for _ in range(9)]
-
-        obj_1 = main.objective_score(pattern=pattern_1, turns=self.turns,
-                                     repetitions=self.repetitions, opponents=opponent,
-                                     params=self.params)
-        obj_2 = main.objective_score(pattern=pattern_2, turns=self.turns,
-                                     repetitions=self.repetitions, opponents=opponent,
-                                     params=self.params)
-        self.assertTrue(np.isclose(abs(obj_1), 5))
-        self.assertTrue(np.isclose(abs(obj_2), 3))
-
-    def test_against_tit_for_tat(self):
-        opponent = [(1, 0, 1, 0)]
-        pattern_1 = [0 for _ in range(9)]
-        pattern_2 = [1 for _ in range(9)]
-
-        obj_1 = main.objective_score(pattern=pattern_1, turns=self.turns,
-                                     repetitions=self.repetitions, opponents=opponent,
-                                     params=self.params)
-        obj_2 = main.objective_score(pattern=pattern_2, turns=self.turns,
-                                     repetitions=self.repetitions, opponents=opponent,
-                                     params=self.params)
-        self.assertTrue(np.isclose(abs(obj_1), 1.04))
-        self.assertTrue(np.isclose(abs(obj_2), 3))
-
-class TestPatternSize(unittest.TestCase):
-
-    def test_example_one(self):
-        params = [1, 1, 1]
-        self.assertEqual(main.pattern_size(params), 8)
-
-    def test_example_two(self):
-        params = [1, 1, 2]
-        self.assertEqual(main.pattern_size(params), 16)
-
-class TestTrainGambler(unittest.TestCase):
-    opponents = [[0, 0, 0, 0], [1, 1, 1, 1]]
-    turns, repetitions = 10, 2
-
-    def test_bayesian(self):
-        axl.seed(0)
-        x, fun = main.train_gambler(method='bayesian', opponents=self.opponents,
-                                    turns=self.turns, repetitions=self.repetitions,
-                                    params=[1, 1, 2],
-                                    method_params ={'n_random_starts' : 10,
-                                                    'n_calls': 15})
-
-        self.assertEqual(len(x), main.pattern_size([1, 1, 2]) + 1)
-        self.assertTrue(np.isclose(fun, 2.9, atol=10 ** -1))
-
-    def test_differential(self):
-        axl.seed(0)
-        x, fun = main.train_gambler(method='differential', opponents=self.opponents,
-                                    turns=self.turns, repetitions=self.repetitions,
-                                    params=[0, 0, 1], method_params={'popsize': 5})
-
-        self.assertEqual(len(x), main.pattern_size([0, 0, 1]) + 1)
-        self.assertTrue(np.isclose(fun, 3.2, atol=10 ** -1))
-
-class TestOptimalMemoryOne(unittest.TestCase):
-    opponents = [[0, 0, 0, 0], [1, 1, 1, 1]]
-    turns, repetitions = 200, 5
-
-    def test_bayesian(self):
-        axl.seed(0)
-        x, theor, simul = main.optimal_memory_one(method='differential', opponents=self.opponents,
-                                                  turns=self.turns, repetitions=self.repetitions,
-                                                  method_params={'popsize': 100})
-
-        self.assertEqual(len(x), 4)
-        self.assertTrue(np.isclose(theor, 3.0, atol=10 ** -2))
-        self.assertTrue(np.isclose(simul, theor, atol=10 ** -2))
-
-    def test_differential(self):
-        axl.seed(0)
-        x, theor, simul = main.optimal_memory_one(method='bayesian', opponents=self.opponents,
-                                                  turns=self.turns, repetitions=self.repetitions,
-                                                  method_params ={'n_random_starts' : 20,
-                                                                  'n_calls': 40})
-
-        self.assertEqual(len(x), 4)
-        self.assertTrue(np.isclose(theor, 3.0, atol=10 ** -2))
-        self.assertTrue(np.isclose(simul, theor, atol=10 ** -1))
 
 class TestWriteFile(unittest.TestCase):
     method = 'bayesian'
