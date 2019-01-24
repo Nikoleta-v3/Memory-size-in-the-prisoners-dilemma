6f7c914c0895f6fb90f7798ffe6686779ab6d628
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-01-24 23:13:15 +0000

    add convergance check

--- tests/test_optimisation.py
+++ tests/test_optimisation.py
@@ -25,17 +25,6 @@ def test_bayesian_mem_one():
     assert np.isclose(theor, 3.0, atol=10 ** -2)
 
 
-# def test_find_evolutionary_best_response():
-#     opponents = [[0, 0, 0, 0], [1, 1, 1, 1]]
-#     opt_player, hist = opt_mo.find_evolutionary_best_response(
-#         opponents, opt_mo.memory_one_best_response
-#     )
-
-#     expected = [0.3647357415723788, 0.030269685380524037, 0.0, 0.0]
-#     assert len(hist) == 2
-#     assert np.allclose(opt_player, expected)
-
-
 def test_find_repeat_in_history_with_single_element():
     history_with_single_element_cycle = (
         np.array([.3, .4, .3, .3]),
@@ -44,10 +33,7 @@ def test_find_repeat_in_history_with_single_element():
         np.array([.3, .4, .3, .3]),
     )
 
-    assert np.array_equal(
-        opt_mo.find_repeat_in_history(history_with_single_element_cycle),
-        (np.array([0.3, 0.4, 0.3, 0.3]),),
-    )
+    assert opt_mo.find_repeat_in_history(history_with_single_element_cycle) == 1
 
 
 def test_find_repeat_in_history_with_two_elements():
@@ -60,10 +46,7 @@ def test_find_repeat_in_history_with_two_elements():
         np.array([.3, .4, .3, .4]),
     )
 
-    assert np.array_equal(
-        opt_mo.find_repeat_in_history(history_with_two_element_cycle),
-        (np.array([0.3, 0.4, 0.3, 0.3]), np.array([0.3, 0.4, 0.3, 0.4])),
-    )
+    assert opt_mo.find_repeat_in_history(history_with_two_element_cycle) == 2
 
 
 def test_find_repeat_in_history_with_three_elements():
@@ -80,14 +63,7 @@ def test_find_repeat_in_history_with_three_elements():
         np.array([.3, .4, .2, .4]),
     )
 
-    assert np.array_equal(
-        opt_mo.find_repeat_in_history(history_with_three_element_cycle),
-        (
-            np.array([0.7, 0.4, 0.3, 0.3]),
-            np.array([0.3, 0.4, 0.3, 0.4]),
-            np.array([0.3, 0.4, 0.2, 0.4]),
-        ),
-    )
+    assert opt_mo.find_repeat_in_history(history_with_three_element_cycle) == 3
 
 
 def test_find_repeat_in_history_with_three_elements_and_floating_error():
@@ -104,15 +80,11 @@ def test_find_repeat_in_history_with_three_elements_and_floating_error():
         np.array([.3, .4, .2, .4]),
     )
 
-    assert np.array_equal(
+    assert (
         opt_mo.find_repeat_in_history(
             history_with_three_element_cycle_and_floating_point_error
-        ),
-        (
-            np.array([0.7, 0.4, 0.3, 0.3]),
-            np.array([0.3, 0.4, 0.3, 0.4]),
-            np.array([0.3, 0.4, 0.2, 0.4]),
-        ),
+        )
+        == 3
     )
 
 
@@ -129,7 +101,7 @@ def test_find_repeat_in_history_no_cycle():
         np.array([.3, .4, .3, .4]),
     )
 
-    assert opt_mo.find_repeat_in_history(history_with_no_cycle) is False
+    assert opt_mo.find_repeat_in_history(history_with_no_cycle) == float("inf")
 
 
 def test_find_repeat_cycle_from_the_beginning():
@@ -140,7 +112,28 @@ def test_find_repeat_cycle_from_the_beginning():
         np.array([.3, .7, .3, .8]),
     )
 
-    assert np.array_equal(
-        opt_mo.find_repeat_in_history(history_with_cycle_from_the_beginning),
-        (np.array([.3, .4, .3, .3]), np.array([.3, .7, .3, .8])),
+    assert (
+        opt_mo.find_repeat_in_history(history_with_cycle_from_the_beginning)
+        == 2
     )
+
+
+def test_objective_is_converge():
+    values = [
+        2.9859256920,
+        3.0269409991,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+        3.0850608205,
+    ]
+
+    assert opt_mo.optimisation.objective_is_converged(values) == True
