03b00b71cbbd7fe24449bde0716aa34f877a45c2
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-02-17 12:30:02 +0000

    fix reactive function call

--- src/opt_mo/parameter_sweep.py
+++ src/opt_mo/parameter_sweep.py
@@ -115,9 +115,9 @@ if __name__ == "__main__":
             dask.delayed(opt_mo.get_evolutionary_best_response)(
                 opponents, opt_mo.get_memory_one_best_response
             ),
-            dask.delayed(opt_mo.get_reactive_best_response)(opponents),
+            dask.delayed(opt_mo.get_reactive_best_response_with_bayesian)(opponents),
             dask.delayed(opt_mo.get_evolutionary_best_response)(
-                opponents, opt_mo.get_reactive_best_response
+                opponents, opt_mo.get_reactive_best_response_with_bayesian
             ),
         ]
 
