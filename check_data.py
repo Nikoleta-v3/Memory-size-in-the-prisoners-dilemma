790b2e3e8540ba318ab8a76cc984d382ad5020ef
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-03-12 10:41:02 +0000

    implement check data

---
+++ check_data.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import opt_mo
+
+
+df = opt_mo.tools.read_sql_data_frame("data/without_gambler/main_seed_two.db")
+df_with_g = opt_mo.tools.read_sql_data_frame(
+    "data/with_gambler/main_seed_two.db"
+)
+
+columns = len(df.columns)
+for index in df_with_g["exp_index"]:
+    assert np.allclose(
+        df_with_g[df_with_g["exp_index"] == index].values[0][:columns],
+        df[df["exp_index"] == index].values,
+    )
