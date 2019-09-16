1fa6f8669ca01ffea4739d6ff6a9f218ed459f90
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2019-09-16 22:11:56 +0100

    new notebook and change in test data

--- src/opt_mo/check_data.py
+++ src/opt_mo/check_data.py
@@ -4,18 +4,14 @@ import sqlalchemy as sa
 
 import opt_mo
 
-engine_df = sa.create_engine("sqlite:///data/without_gambler/main.db")
-df = pd.read_sql("experiments", engine_df.connect())
+df = pd.read_csv("data/numerical_experiments_without_gambler.csv")
 
 
-engine_df_with_gambler = sa.create_engine("sqlite:///data/with_gambler/main.db")
-df_with_gambler = pd.read_sql("experiments", engine_df_with_gambler.connect())
+gambler = pd.read_csv("data/numerical_experiments_without_gambler.csv")
 
 columns = len(df.columns)
-for index in df_with_gambler["exp_index"]:
+for index in gambler["exp_index"]:
     assert np.allclose(
-        df_with_gambler[df_with_gambler["exp_index"] == index].values[0][
-            :columns
-        ],
+        gambler[gambler["exp_index"] == index].values[0][:columns],
         df[df["exp_index"] == index].values,
     )
